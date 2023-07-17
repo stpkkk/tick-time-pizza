66d8e3de0d9679dd94c9f5ccea1c523aec9176b7
Igor Stepanov <stipyk1309@gmail.com>
2023-07-18 02:07:07 +0300

    menu data add ingredients, categories, options, sizes, nutritional

--- types/menu.ts
+++ types/menu.ts
@@ -1,9 +1,31 @@
 import { StaticImageData } from "next/image";
 
+export interface additionalIngredientsTypes {
+  id: number;
+  image: string | StaticImageData;
+  name: string;
+  weight: number;
+  price: number;
+}
+
+export interface nutritionalValueTypes {
+  proteins: number;
+  fats: number;
+  carbohydrates: number;
+  calories: number;
+}
+
 export interface MenuItemTypes {
   id: number;
   title: string;
   image: string | StaticImageData;
   ingredients: string;
   price: number;
+  additionalIngredients: additionalIngredientsTypes[];
+  removeIngredients: string[];
+  categories?: string[];
+  categoryImages?: StaticImageData[];
+  sizes: number[];
+  options: string[];
+  nutritionalValue: nutritionalValueTypes;
 }
