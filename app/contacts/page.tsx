46ae362c7e73ac2bb2b10ff84c38d94f17a0a65e
Igor Stepanov <stipyk1309@gmai.com>
2023-10-05 17:59:54 +0300

    Contacts add map

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -24,7 +24,7 @@ const Contacts: React.FC = () => {
         {pizzerias.map((pizzeria) => (
           <Pizzerias pizzeria={pizzeria} />
         ))}
-        <p className='sm:text-sm text-base sm:mb-30px mb-50px'>
+        <p className='sm:text-sm text-base sm:mb-[30px] mb-[50px]'>
           Телефон для заказа пиццы, еды и напитков в "Тик Тайм":{' '}
           <span>
             <a className='underline' href='tel:330204'>
@@ -43,6 +43,39 @@ const Contacts: React.FC = () => {
             ticktime.mail@gmail.com
           </a>
         </p>
+        <div className='rounded-2xl overflow-hidden'>
+          <a
+            href='https://yandex.ru/maps/18/petrozavodsk/?utm_medium=mapframe..'
+            style={{
+              color: 'rgb(238, 238, 238)',
+              fontSize: '12px',
+              position: 'absolute',
+              top: '0px',
+            }}
+          >
+            Петрозаводск
+          </a>
+          <a
+            href='https://yandex.ru/maps/18/petrozavodsk/?from=api-maps&amp;ll=..'
+            style={{
+              color: 'rgb(238, 238, 238)',
+              fontSize: '12px',
+              position: 'absolute',
+              top: '14px',
+            }}
+          >
+            Яндекс.Карты — поиск мест и адресов, городской транспорт
+          </a>
+          <iframe
+            src='https://yandex.ru/map-widget/v1/-/CCUEiSBOwD'
+            width='100%'
+            height='400'
+            // allowfullscreen=''
+            style={{
+              position: 'relative',
+            }}
+          ></iframe>
+        </div>
       </div>
     </div>
   );
