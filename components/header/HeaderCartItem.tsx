42fede9a654875be891d548f969329f82a5c7fca
Igor Stepanov <stipyk1309@gmail.com>
2023-11-02 18:38:24 +0300

    HeaderCartItem refactoring

--- components/header/HeaderCartItem.tsx
+++ components/header/HeaderCartItem.tsx
@@ -20,19 +20,42 @@ const HeaderCartItem: React.FC<HeaderCartItemProps> = ({
     product.productQuantity || 1,
   );
 
-  const renderPromoProducts = (products: IProduct[]) => {
-    return (
-      <ul className='cart_ingredients flex-col'>
-        {products.map((product) => (
-          <li key={generateUUID()}>
-            {product.title}&nbsp;{product.selectedSize?.name},&nbsp;
-            {product.selectedDough?.name}&nbsp;(
-            {product.productQuantity}шт.)
-          </li>
-        ))}
-      </ul>
-    );
-  };
+  const renderPromoProducts = (products: IProduct[]) => (
+    <ul className='cart_ingredients flex-col'>
+      {products.map((product) => (
+        <li key={generateUUID()}>
+          {`${product.title} ${product.selectedSize?.name}, ${product.selectedDough?.name} (${product.productQuantity} шт.)`}
+        </li>
+      ))}
+    </ul>
+  );
+
+  const renderIngredients = () => (
+    <div>
+      {product.selectedSize?.name && product.selectedDough?.name && (
+        <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
+          {`${product.selectedSize?.name},${product.selectedDough?.name}`}
+        </p>
+      )}
+      {product.removedIngredients && product.removedIngredients.length > 0 && (
+        <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
+          Убрать:{' '}
+          {product.removedIngredients?.map((ing) => (
+            <span key={ing.id}>{`${ing.name}, `}</span>
+          ))}
+        </p>
+      )}
+      {product.selectedIngredients &&
+        product.selectedIngredients.length > 0 && (
+          <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
+            Добавить:{' '}
+            {product.selectedIngredients.map((ing) => (
+              <span key={ing.id}>{`${ing.name} (${ing.quantity}шт.), `}</span>
+            ))}
+          </p>
+        )}
+    </div>
+  );
 
   return (
     <div>
@@ -41,10 +64,10 @@ const HeaderCartItem: React.FC<HeaderCartItemProps> = ({
           {product.title}
         </p>
         <p className='whitespace-nowrap text-[14px] md:text-sm'>
-          {product.productQuantity} шт.
+          {`${product.productQuantity} шт.`}
         </p>
         <p className='w-10 whitespace-nowrap text-center text-[14px] font-semibold md:w-16 md:text-sm'>
-          {totalProductPrice} ₽
+          {`${totalProductPrice} ₽`}
         </p>
         <button type='button' onClick={() => onRemove(product.uuid || '')}>
           <RiDeleteBin6Line
@@ -53,37 +76,9 @@ const HeaderCartItem: React.FC<HeaderCartItemProps> = ({
           />
         </button>
       </div>
-      {promoProducts && promoProducts.length > 0 ? (
-        renderPromoProducts(promoProducts)
-      ) : (
-        <div>
-          {product.selectedSize?.name && product.selectedDough?.name && (
-            <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
-              {product.selectedSize?.name},{product.selectedDough?.name}
-            </p>
-          )}
-          {product.removedIngredients &&
-            product.removedIngredients.length > 0 && (
-              <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
-                Убрать:&nbsp;
-                {product.removedIngredients?.map((ing) => (
-                  <span key={ing.id}>{ing.name},&nbsp;</span>
-                ))}
-              </p>
-            )}
-          {product.selectedIngredients &&
-            product.selectedIngredients.length > 0 && (
-              <p className='break-words text-[12px] font-normal md:text-xs md:leading-[15px]'>
-                Добавить:&nbsp;
-                {product.selectedIngredients.map((ing) => (
-                  <span key={ing.id}>
-                    {ing.name}&nbsp;({ing.quantity}шт.),&nbsp;
-                  </span>
-                ))}
-              </p>
-            )}
-        </div>
-      )}
+      {promoProducts && promoProducts.length > 0
+        ? renderPromoProducts(promoProducts)
+        : renderIngredients()}
     </div>
   );
 };
