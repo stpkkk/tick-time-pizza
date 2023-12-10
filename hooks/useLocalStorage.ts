6c9095f7e74082b9913882cdd60aa556706a5392
Igor Stepanov <stipyk1309@gmail.com>
2023-12-10 16:45:24 +0300

    LocalStorage fix error in terminal and in deployment

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -3,32 +3,28 @@
 import React from 'react';
 import { ExtendedUser } from '@/types';
 
-function useLocalStorage(initialValue: [] | ExtendedUser, key: string) {
-  const [state, setState] = React.useState(() => {
-    // Initialize the state
-    try {
-      const value = window.localStorage.getItem(key);
-      // Check if the local storage already has any values,
-      // otherwise initialize it with the passed initialValue
-      return value ? JSON.parse(value) : initialValue;
-    } catch (error) {
-      console.log(error);
-    }
-  });
+function useLocalStorage(initialValue: ExtendedUser | [], key: string) {
+  const isClient = typeof window !== 'undefined';
 
-  const setValue = (value: (arg0: any) => any) => {
-    try {
-      // If the passed value is a callback function,
-      //  then call it with the existing state.
-      const valueToStore = value instanceof Function ? value(state) : value;
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-      setState(value);
-    } catch (error) {
-      console.log(error);
+  const getValue = () => {
+    if (isClient) {
+      const storedItem = localStorage.getItem(key);
+      if (storedItem) {
+        return JSON.parse(storedItem);
+      }
     }
+    return initialValue;
   };
 
-  return [state, setValue];
+  const [value, setValue] = React.useState(() => getValue());
+
+  React.useEffect(() => {
+    if (isClient) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [isClient, key, value]);
+
+  return [value, setValue];
 }
 
 export default useLocalStorage;
