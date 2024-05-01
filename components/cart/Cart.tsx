37aaf417657df223f38c09a10e680f0b011b45d2
Igor Stepanov <stipyk1309@gmail.com>
2024-05-01 18:39:20 +0300

    fix: Cart remove unused type

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -29,7 +29,7 @@ const Cart: React.FC = () => {
     productUUID: string,
     modifier: (product: IProduct) => IProduct,
   ) => {
-    const updatedItems = cartProducts.map((product: IProduct) =>
+    const updatedItems = cartProducts.map((product) =>
       product.uuid === productUUID ? modifier(product) : product,
     );
     updateItemsInLocalStorage(updatedItems);
@@ -64,7 +64,7 @@ const Cart: React.FC = () => {
       <div className='md:max-w-full w-full'>
         {cartProducts.length > 0 ? (
           <ul className='wrapper flex flex-col gap-[30px] px-[60px] py-[50px] sm:px-4 sm:py-8'>
-            {cartProducts.map((product: IProduct) => (
+            {cartProducts.map((product) => (
               <CartProduct
                 key={product.uuid}
                 product={product}
